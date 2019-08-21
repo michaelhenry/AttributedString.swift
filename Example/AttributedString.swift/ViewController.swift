098c24dd7103e3c056ee98a3e8d290c7b2f6c2e9
Michael Henry Pantaleon <me@iamkel.net>
2019-08-22 02:08:42 +0900

    update example

--- Example/AttributedString.swift/ViewController.swift
+++ Example/AttributedString.swift/ViewController.swift
@@ -7,18 +7,56 @@
 //
 
 import UIKit
+import AttributedString_swift
 
 class ViewController: UIViewController {
+  
+  @IBOutlet weak var label1:UILabel!
+  @IBOutlet weak var label2:UILabel!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    let attrs1:[NSAttributedString.Attribute] = [
+      .font(UIFont.boldSystemFont(ofSize: 30)),
+      .link("https://www.google.com")
+    ]
+    
+    label1.attributedText = NSAttributedString(
+      string: "Hello World", attrs:attrs1)
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    let attrText2 = NSMutableAttributedString(
+      string: "The quick brown fox jump over the lazy dog.",
+      attrs: [
+        .font(UIFont.systemFont(ofSize: 18)),
+        .foregroundColor(UIColor.orange)
+      ])
+    
+    // for The
+    attrText2.addAttributes(
+      attrs: [
+        .font(UIFont.boldSystemFont(ofSize: 50)),
+        .foregroundColor(UIColor.gray)
+      ],
+      for: .subtext("The"))
+    
+    // For dog
+    attrText2.addAttributes(
+      attrs: [
+        .font(UIFont.boldSystemFont(ofSize: 30)),
+        .link("https://en.wikipedia.org/wiki/Dog"),
+      ],
+      for: .subtext("dog"))
+    
+    // With paragraph style for whole text
+    let paragraphStyle = NSMutableParagraphStyle()
+    paragraphStyle.alignment = .center
+    attrText2.addAttributes(
+      attrs: [
+        .paragraphStyle(paragraphStyle)
+      ],
+      for: .all)
+    label2.attributedText = attrText2
+  }
 }
+
 
